aeafbfc1c7146766eb842540a0843f87f3531b80
eviktor86 <torok.viktor86@gmail.com>
2017-02-15 03:35:24 +0100

    Add files via upload

---
+++ ReadingRawInput.py
@@ -0,0 +1,10 @@
+'''
+Title     : Reading Raw Input
+Subdomain : Introduction
+Domain    : Python
+Author    : Viktor Török
+Created   : 15 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+s = raw_input()
+print s
