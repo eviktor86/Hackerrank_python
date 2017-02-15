448453fc8293992f69927672c317dcc176a54813
eviktor86 <torok.viktor86@gmail.com>
2017-02-15 03:38:06 +0100

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+'''
+Title     : Hello, World!
+Subdomain : Introduction
+Domain    : Python
+Author    : Viktor Török
+Created   : 15 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+my_string = "Hello, World!"
+print(my_string)
