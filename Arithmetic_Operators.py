72a6768e1c87327c0114b5048b9d948806d4d3ad
eviktor86 <torok.viktor86@gmail.com>
2017-02-15 04:28:40 +0100

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,14 @@
+'''
+Title     : Arithmetic Operators
+Subdomain : Introduction
+Domain    : Python
+Author    : Viktor Török
+Created   : 15 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print (a + b)
+    print (a - b)
+    print (a * b)
