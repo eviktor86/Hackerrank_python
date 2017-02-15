31661ad2a2d1f66832c95feb3f4d8da497121ec3
eviktor86 <torok.viktor86@gmail.com>
2017-02-15 04:41:03 +0100

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,14 @@
+'''
+Title     : Division
+Subdomain : Introduction
+Domain    : Python
+Author    : Viktor Török
+Created   : 15 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import division
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print (a // b)
+    print (a / b)
---
+++ Loops.py
@@ -0,0 +1,14 @@
+'''
+Title     : Division
+Subdomain : Loops
+Domain    : Python
+Author    : Viktor Török
+Created   : 15 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n = int(raw_input())
+    i = 0
+while i < n:
+    print (i * i)
+    i += 1
