2665058800ea1d6d3121465665c9bc11813ac1ec
eviktor86 <torok.viktor86@gmail.com>
2017-02-17 05:05:07 +0100

    Add files via upload

---
+++ PrintFunction.py
@@ -0,0 +1,10 @@
+'''
+Title     : Print function
+Subdomain : Introduction
+Domain    : Python
+Author    : Viktor Török
+Created   : 17 02 2017
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function
+print(*range(1, int(raw_input())+1), sep='')
